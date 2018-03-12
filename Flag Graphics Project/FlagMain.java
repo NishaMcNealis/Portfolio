940040966d791c05ea9f1d8978cea492175dc3f5
NishaMcNealis <nisha.mcnealis@gmail.com>
2018-03-11 22:31:28 -0700

    Add files via upload

---
+++ Flag Graphics Project/FlagMain.java
@@ -0,0 +1,15 @@
+package completed;
+/* Flag project main
+ * Nisha McNealis
+ * AP CS Period 3
+ * 8/31/16
+ */
+public class FlagMain {
+		public static void main(String[] args) {
+			Flag f = new Flag();
+			f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+			f.setVisible(true);
+		}
+
+	}
+
